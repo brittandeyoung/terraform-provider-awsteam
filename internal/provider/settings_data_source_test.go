754699a2cc7a10ca6d7c526f6106f8ad118c6408
Brittan DeYoung <32572259+brittandeyoung@users.noreply.github.com>
2024-01-03 18:59:17 -0500

    provider: Amend settings_data_source_test.go, move to resource based testing

--- internal/provider/settings_data_source_test.go
+++ internal/provider/settings_data_source_test.go
@@ -1,43 +1,57 @@
 package provider
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-func TestAccSettingsDataSource(t *testing.T) {
-	dataSource := "data.awsteam_settings.test"
+func testAccSettingsDataSource_basic(t *testing.T) {
+	resourceName := "awsteam_settings.test"
+	dataSourceName := "data.awsteam_settings.test"
+	teamAdminGroup := "Team-Admin-Group"
+	teamAuditorGroup := "Team-Auditor-Group"
+	duration := rand.Intn(10)
+	expiry := rand.Intn(10)
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccSettingsDataSourceConfig,
+				Config: testAccSettingsDataSourceConfig(teamAdminGroup, teamAuditorGroup, duration, expiry),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(dataSource, "id", "settings"),
-					// resource.TestCheckResourceAttrSet(dataSource, "approval"),
-					// resource.TestCheckResourceAttrSet(dataSource, "comments"),
-					// resource.TestCheckResourceAttrSet(dataSource, "max_request_duration"),
-					// resource.TestCheckResourceAttrSet(dataSource, "request_expiry_timeout"),
-					// resource.TestCheckResourceAttrSet(dataSource, "ses_notifications_enabled"),
-					// resource.TestCheckResourceAttrSet(dataSource, "sns_notifications_enabled"),
-					// resource.TestCheckResourceAttrSet(dataSource, "slack_notifications_enabled"),
-					// resource.TestCheckResourceAttrSet(dataSource, "ses_source_email"),
-					// resource.TestCheckResourceAttrSet(dataSource, "ses_source_arn"),
-					// resource.TestCheckResourceAttrSet(dataSource, "slack_token"),
-					// resource.TestCheckResourceAttrSet(dataSource, "team_admin_group"),
-					// resource.TestCheckResourceAttrSet(dataSource, "team_auditor_group"),
-					// resource.TestCheckResourceAttrSet(dataSource, "ticket_no"),
-					// resource.TestCheckResourceAttrSet(dataSource, "modified_by"),
-					// resource.TestCheckResourceAttrSet(dataSource, "created_at"),
-					// resource.TestCheckResourceAttrSet(dataSource, "updated_at"),
+					resource.TestCheckResourceAttrPair(resourceName, "id", dataSourceName, "id"),
+					resource.TestCheckResourceAttrPair(resourceName, "team_admin_group", dataSourceName, "team_admin_group"),
+					resource.TestCheckResourceAttrPair(resourceName, "team_auditor_group", dataSourceName, "team_auditor_group"),
+					resource.TestCheckResourceAttrPair(resourceName, "approval", dataSourceName, "approval"),
+					resource.TestCheckResourceAttrPair(resourceName, "comments", dataSourceName, "comments"),
+					resource.TestCheckResourceAttrPair(resourceName, "ses_notifications_enabled", dataSourceName, "ses_notifications_enabled"),
+					resource.TestCheckResourceAttrPair(resourceName, "sns_notifications_enabled", dataSourceName, "sns_notifications_enabled"),
+					resource.TestCheckResourceAttrPair(resourceName, "slack_notifications_enabled", dataSourceName, "slack_notifications_enabled"),
+					resource.TestCheckResourceAttrPair(resourceName, "ticket_no", dataSourceName, "ticket_no"),
+					resource.TestCheckResourceAttrPair(resourceName, "created_at", dataSourceName, "created_at"),
+					resource.TestCheckResourceAttrPair(resourceName, "updated_at", dataSourceName, "updated_at"),
 				),
 			},
 		},
 	})
 }
+func testAccSettingsDataSourceConfig(teamAdminGroup string, teamAuditorGroup string, duration int, expiry int) string {
+	return fmt.Sprintf(`
+resource "awsteam_settings" "test" {
+	team_admin_group = %[1]q
+	team_auditor_group = %[2]q
+	duration = %d
+	expiry = %d
+  }
 
-const testAccSettingsDataSourceConfig = `
-data "awsteam_settings" "test" {}
-`
+data "awsteam_settings" "test" {
+	depends_on = [
+		awsteam_settings.test
+	]
+}
+`, teamAdminGroup, teamAuditorGroup, duration, expiry)
+}
