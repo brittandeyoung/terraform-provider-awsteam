46f2d7cbe033a3a3f6e6a060e815875eb220368c
Brittan DeYoung <32572259+brittandeyoung@users.noreply.github.com>
2024-01-17 09:29:34 -0500

    provider: Add consts and shared schema attributes for eligibilities

--- internal/names/attr_consts.go
+++ internal/names/attr_consts.go
@@ -1,7 +1,10 @@
 package names
 
 const (
-	AttrModifiedBy = "modified_by"
-	AttrCreatedAt  = "created_at"
-	AttrUpdatedAt  = "updated_at"
+	AttrModifiedBy    = "modified_by"
+	AttrCreatedAt     = "created_at"
+	AttrUpdatedAt     = "updated_at"
+	AttrAccountSet    = "accounts"
+	AttrOUSet         = "ous"
+	AttrPermissionSet = "permissions"
 )
