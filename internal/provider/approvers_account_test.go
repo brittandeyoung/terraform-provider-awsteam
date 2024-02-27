7895356239c04701598d410270353e8c3d8601e3
Brittan DeYoung <32572259+brittandeyoung@users.noreply.github.com>
2024-02-27 11:13:56 -0500

    provider: approvers_account_test, Add test to verify account with leading zero

--- internal/provider/approvers_account_test.go
+++ internal/provider/approvers_account_test.go
@@ -49,10 +49,42 @@ func TestAccApproversAccountResource_basic(t *testing.T) {
 	})
 }
 
+func TestAccApproversAccountResource_accountId(t *testing.T) {
+	resourceName := "awsteam_approvers_account.test"
+	accountIdLeadingZeros := "000000123456"
+	accountName := gofakeit.BS()
+	approver1 := gofakeit.Email()
+	groupId1 := gofakeit.UUID()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccApproversAccountResourceConfig(accountIdLeadingZeros, accountName, approver1, groupId1),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "account_id", accountIdLeadingZeros),
+					resource.TestCheckResourceAttr(resourceName, "id", accountIdLeadingZeros),
+					resource.TestCheckResourceAttr(resourceName, "account_name", accountName),
+					resource.TestCheckTypeSetElemAttr(resourceName, "approvers.*", approver1),
+					resource.TestCheckTypeSetElemAttr(resourceName, "group_ids.*", groupId1),
+					resource.TestCheckResourceAttrSet(resourceName, "created_at"),
+					resource.TestCheckResourceAttrSet(resourceName, "updated_at"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func testAccApproversAccountResourceConfig(accountId, accountName, approver, groupId string) string {
 	return fmt.Sprintf(`
 resource "awsteam_approvers_account" "test" {
-	account_id   = %[1]s
+	account_id   = %[1]q
 	account_name = %[2]q
 	approvers = [
 		%[3]q
